07cbba8d1bb36adbf817d1f8ffaef7159d0ce2bb
Saotome <saotome@usp.br>
2021-05-11 14:49:40 -0300

    Implementação do ImportaCommand

--- app/Console/Commands/ImportaCommand.php
+++ app/Console/Commands/ImportaCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Termo;
 use App\Models\Remissiva;
+# Namespace da tabela pivot
 
 class ImportaCommand extends Command
 {
@@ -43,12 +44,28 @@ class ImportaCommand extends Command
         foreach($csv as $row){
             $termo = new Termo;
             $termo->assunto = $row[0];
+            $termo->observacao = $row[3];
+            $termo->categoria = $row[4];
+            $termo->enviado_para_sibi = $row[5];
+            $termo->normalizado = $row[6];
             $termo->save();
 
             if(!empty($row[7])){
                 $remissa = new Remissiva;
                 $remissa->titulo = $row[7];
                 $remissa->termo_id = $termo->id;
+            }
+
+            if(!empty($row[1])){
+                $cdd = new Cdd;
+                $cdd->cdd = $row[1];
+                $cdd->save();
+
+                # tabela pivot
+                $cdd_termo = new CddTermo
+                $cdd_termo->cdd_id = $cdd->id;
+                $cdd_termo->termo_id = $termo->id;
+                $cdd_termo->save();
             }
         }
 
