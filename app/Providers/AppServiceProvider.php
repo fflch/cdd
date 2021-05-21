191c0aea84fd05595ab424db7827aa6821fe94bf
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-05-21 17:47:23 -0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
